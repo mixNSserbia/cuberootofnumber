ff01acfe2fa30bcd7373f71595018f84d2270b83
mixNSserbia <miloradmarkov@gmail.com>
2023-05-04 15:45:36 +0200

    Add files via upload

---
+++ cuberootnumber.cpp
@@ -0,0 +1,12 @@
+// program to find cube root of a nuber in c++
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+  float number, ans;
+  cout << "Enter any number: ";
+  cin >> number;
+  ans = cbrt(number);
+  cout << "\n Cube Root of " << number << " is: " << ans;
+}
